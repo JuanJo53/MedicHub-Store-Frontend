83388bf3139ea9a13f4523ddda4b53e773610593
XimeCruz <xmenacruzt@gmail.com>
2021-05-13 11:42:43 -0400

    Table products

--- src/app/modules/pharm-admin/pages/pharm-dashboard/pharm-dashboard.component.ts
+++ src/app/modules/pharm-admin/pages/pharm-dashboard/pharm-dashboard.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatDialog, MatPaginator, MatSort, MatTableDataSource } from "@angular/material";
 import { ChartDataSets, ChartOptions, ChartType } from "chart.js";
 import { Color, BaseChartDirective, Label, MultiDataSet } from "ng2-charts";
+import { TokenService } from "src/app/core/authentication/token.service";
+import { PharmOrderService } from "src/app/core/http/pharm-admin/pharmOrder.service";
+import { OrderProductsComponent } from "src/app/modules/client/components/dialogs/order-products/order-products.component";
+import { Order } from "src/app/shared/models/order";
 @Component({
   selector: "app-pharm-dashboard",
   templateUrl: "./pharm-dashboard.component.html",
@@ -91,7 +96,38 @@ export class PharmDashboardComponent implements OnInit {
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
 
-  constructor() {}
+  //Table
+  /*orders: Order[] = [];
+
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  length = 1;
+  size = 18;
+  order = "id";
+  asc = true;
+  actualPage = 0;
+
+  displayedColumns: string[] = [
+    "id_sale",
+    "Name",
+    "Cost",
+    "Quantity",
+  ];
+
+  id: number;
+  typeOrder: any;
+  filter: any;
+  filterType: any;
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;*/
+
+  constructor(
+    /*public dialog: MatDialog,
+    private orderService: PharmOrderService,
+    private tokenService: TokenService*/
+  ) {}
 
   ngOnInit(): void {}
 
@@ -145,4 +181,55 @@ export class PharmDashboardComponent implements OnInit {
   public changeLabel(): void {
     this.lineChartLabels[2] = ["1st Line", "2nd Line"];
   }
+  //table
+
+  /*public tabledatasource(): void {
+    this.id = parseInt(this.tokenService.getSubsidiaryId());
+    try {
+      if (this.id) {
+        this.typeOrder = "2";
+        this.fecthOrders(this.length);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+  dataSource = new MatTableDataSource();
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  fecthOrders(page: number): void {
+    this.orderService
+      .getSubsidiaryOrders(
+        this.id,
+        page,
+        this.size,
+        parseInt(this.typeOrder),
+        this.filter,
+        this.filterType
+      )
+      .subscribe((orders) => {
+        this.orders = orders;
+        this.length = orders[0].size;
+        this.dataSource = new MatTableDataSource(this.orders);
+        this.dataSource.sort = this.sort;
+        this.isLoadingResults = false;
+      });
+  }
+
+  refreshOrders(event) {
+    this.actualPage = event.pageIndex;
+    this.fecthOrders(event.pageIndex + 1);
+  }
+  productsView(products: any) {
+    this.dialog.open(OrderProductsComponent, {
+      width: "2050px",
+      data: {
+        products: products,
+      },
+    });
+  }*/
 }
